a9e2c6255646c29a0771210ad15d9e3fcdd0ca98
ghostjaby assil <altf4irl@gmail.com>
2024-06-09 19:52:12 +0300

    Submission of exercise 9.9.

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ app.use(express.json());
 
 const PORT: number = 3000;
 
+app.get('/ping', (_req, res) => {
+    res.send('Pong');
+});
+
 app.listen(PORT, () => {
   console.log(`Server up un running on port ${PORT}`);
 });
