845f908d0e0ad481ec9ba4bd0dff62e363b02e41
ghostjaby assil <altf4irl@gmail.com>
2024-06-14 13:26:49 +0300

    Submission of Exercise 9.24.

--- src/routes/diagnoses.ts
+++ src/routes/diagnoses.ts
@@ -3,9 +3,19 @@ import diagnosisService from '../services/dignosisService';
 
 const router = express.Router();
 
-
 router.get('/', (_req, res) => {
-    res.json(diagnosisService.getDiagnoses());
+  res.json(diagnosisService.getDiagnoses());
+});
+
+router.get('/:code', (req, res) => {
+  try {
+    const diag = diagnosisService.getDiagnosisByCode(req.params.code);
+    res.json(diag);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(404).json({ error: err.message });
+    }
+  }
 });
 
 export default router;
--- src/services/dignosisService.ts
+++ src/services/dignosisService.ts
@@ -2,9 +2,19 @@ import diagnoses from '../../data/diagnoses';
 import { Diagnosis } from '../types';
 
 const getDiagnoses = (): Diagnosis[] => {
-    return diagnoses;
+  return diagnoses;
+};
+
+const getDiagnosisByCode = (code: string): Diagnosis => {
+  const diagnosis = diagnoses.find((diagnosis) => diagnosis.code === code);
+  if (diagnosis) {
+    return diagnosis;
+  } else {
+    throw new Error('code doesnt exist');
+  }
 };
 
 export default {
-    getDiagnoses
+  getDiagnoses,
+  getDiagnosisByCode,
 };
